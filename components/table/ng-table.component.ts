00a261ac7f488837c070b46d725ed88991a1bf98
Nikhil N <nikhiln@users.noreply.github.com>
2016-10-18 19:21:08 +0530

    Changed to material design icons

--- components/table/ng-table.component.ts
+++ components/table/ng-table.component.ts
@@ -11,8 +11,8 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
           <th *ngFor="let column of columns" [ngTableSorting]="config" [column]="column" 
               (sortChanged)="onChangeTable($event)" ngClass="{{column.className || ''}}">
             {{column.title}}
-            <i *ngIf="config && column.sort" class="pull-right fa"
-              [ngClass]="{'fa-chevron-down': column.sort === 'desc', 'fa-chevron-up': column.sort === 'asc'}"></i>
+            <i *ngIf="config && column.sort" class="pull-right mdi"
+              [ngClass]="{'mdi-chevron-down': column.sort === 'desc', 'mdi-chevron-up': column.sort === 'asc'}"></i>
           </th>
         </tr>
       </thead>
